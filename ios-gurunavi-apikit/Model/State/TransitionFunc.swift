77994225b2f49edb721a087a05484d6596b16924
yuu ogasawara <y.ogasawara@st-ventures.jp>
2017-12-02 10:38:33 +0900

    Add Monadic automaton

---
+++ ios-gurunavi-apikit/Model/State/TransitionFunc.swift
@@ -0,0 +1,21 @@
+//
+//  TransitionFunc.swift
+//  ios-gurunavi-apikit
+//
+//  Created by yogasawara@stv on 2017/12/01.
+//  Copyright © 2017年 Sunday Carpenter. All rights reserved.
+//
+
+struct TransitionFunc {
+    //遷移関数
+    //引数はあったりなかったり
+    //返り値はState<RegistrationState,UIViewController> :State Monad
+    //(A?) -> (S,B)
+//    static let apiRootTransition: () -> State<GApiState, UIViewController> = {
+//        let vc = RegisterRootViewController.make(withDependency: ())
+//        return State<RState, UIViewController> { s in
+//            let s1: RState = s == .root ? .registerRoot: .any
+//            return (s1, vc)
+//        }
+//    }
+}
